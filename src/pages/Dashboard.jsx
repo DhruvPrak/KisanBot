f542d8e84448c83ec41116b343927509ab4d7cc8
DhruvPrak <160715886+DhruvPrak@users.noreply.github.com>
2026-08-01 21:48:44 +0530

    fix: suppress exhaustive-deps warning to unblock production build

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,6 +23,7 @@ function Dashboard({ darkMode, setDarkMode }) {
 
   useEffect(() => {
     fetchQueries();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchQueries = async () => {
